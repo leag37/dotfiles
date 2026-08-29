2b88969c37a30b6b3b84564a561f8ddd7807ec86
Gael Huber <gael.scott.huber@gmail.com>
2026-08-29 14:13:28 -0700

    dap virtual text

---
+++ .config/nvim/lua/gael/plugins/debug/nvim-dap-virtual-text.lua
@@ -0,0 +1,15 @@
+return {
+  "theHamsta/nvim-dap-virtual-text",
+  event = "VeryLazy",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    local dap_virtual_text = require("nvim-dap-virtual-text")
+    dap_virtual_text.setup({
+      -- commented = true,
+      highlight_changed_variables = true,
+    })
+  end,
+}
